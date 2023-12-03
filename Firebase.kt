d4609ddd38724082971fdef82d93d10ed7c7e89b
Harshavardhan2903 <89390938+Harshavardhan2903@users.noreply.github.com>
2023-12-03 23:01:13 +0530

    Create Firebase.kt
    This is the file which can be used for help to read and write data from firebase realtime database

---
+++ Firebase.kt
@@ -0,0 +1,15 @@
+//This file can be used for handling firebase related things
+//Here we use realtime firebase as it has onvalueevnt change , which triggers recomposition .
+
+
+
+//To read from firebase 
+fun read_data_from_firebase(){
+  
+}
+
+
+//To write to firebase
+fun write_data_to_firebase(){
+
+}
