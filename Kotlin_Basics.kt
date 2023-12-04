ae1b395021a3ccd0b08895b0847a0099b8083b52
Harshavardhan2903 <89390938+Harshavardhan2903@users.noreply.github.com>
2023-12-04 19:26:21 +0530

    Create Kotlin_Basics.kt

---
+++ Kotlin_Basics.kt
@@ -0,0 +1,20 @@
+//variable declaration
+
+val applicationcontext : Context = applicationcontext
+//means val read only , a variable applicationcontext of type Context
+//Note it is not class , so don't think extends Context
+
+
+
+//function declaration
+fun accept(k : Int , list : List<Int>) : String{
+    println("$k is the number")
+    var gg = "hello"
+    return gg
+}
+
+//for functions we need to explicitly tell the type of variable
+//and for return a string you need to declare , default is unit
+
+
+clean architecture says domain , datalayer and presentation
